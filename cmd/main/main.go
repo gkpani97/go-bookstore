15740595f0da45e133e7978c03e1e65150113bc7
Gurlop Kishore Pani <gkpani97@gmail.com>
2023-10-16 17:48:28 +0530

    Added models, controllers

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,16 @@
+package main
 
+import (
+	"log"
+	"net/http"
+	"github.com/gkpani97/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "gorm.io/driver/mysql"
+)
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,14 +2,14 @@ package utils
 
 import (
 	"net/http"
-	"io/ioutil"
+	"io"
 	"encoding/json"
 )
 
-func ParseBody(r *http.Request, X interface{}){
-	if body, err :=  ioutil.ReadAll(r.Body); err != nil{
+func ParseBody(r *http.Request, x interface{}){
+	if body, err :=  io.ReadAll(r.Body); err != nil{
 		if err := json.Unmarshal([]byte(body), x); err != nil{
 			return
 		}
 	}
-} 
+}
